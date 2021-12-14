f0c452c157a507d7b186a80a72d86819eef49c60
napoleonwar <lizixuan.edu@163.com>
2021-12-14 11:49:45 +0100

    template functions

--- avalon/src/main/scala/avalon_mm_bfm_master.scala
+++ avalon/src/main/scala/avalon_mm_bfm_master.scala
@@ -1,57 +1,89 @@
 package avalon_mm
 
+import Chisel.{Enum, switch}
 import chisel3._
 import chiseltest._
 import chisel3.iotesters._
 import chisel3.util
+import com.sun.org.apache.bcel.internal.generic.SWITCH
 
-class avalon_mm_bfm_master_if(val data_w: Int, val addr_w: Int ) extends Bundle {
+import javax.swing.OverlayLayout
+
+class avalon_mm_bfm_master_if(val data_w: Int, val addr_w: Int, burst_w: Int  ) extends Bundle {
     //Write Host -> agent
     val avm_address = Output(UInt(data_w.W))
     val avm_byteenable = Output(UInt((data_w/8).W))
-    val avm_debugaccess = Output(Bool())
+    //val avm_debugaccess = Output(Bool())
     val avm_read = Output(Bool())
     val avm_write = Output(Bool())
     val avm_writedata = Output(UInt(data_w.W))
+    val avm_lock = Output(Bool())
 
     //Read Agent -> Host
     val avm_readdata = Input(UInt(data_w.W))
     val avm_response = Input(UInt(2.W))
     val avm_waitrequest  = Input(Bool())
-}
-class avalon_mm_bfm_master_if_pipeline(val data_w: Int, val addr_w: Int ) extends Bundle{
-    val avm_if = new avalon_mm_bfm_master_if(data_w, addr_w)
-    // Read Agent -> Host
+    //Read Agent -> Host Pipelined
     val avm_readdatavalid: Bool = Input(Bool())
     val avm_writeresponsevalid: Bool = Input(Bool())
-}
-class avalon_mm_bfm_master_if_burst(val data_w: Int, val addr_w: Int, val burst_w: Int ) extends Bundle {
-    val avm_if = new avalon_mm_bfm_master_if(data_w, addr_w)
-    // Write Host -> Agent
+    //Write Host -> Agent Burst
     val avm_burstcount: Nothing = Output(UInt(burst_w.W))
+}
+class avm_mm_response_status{
+    val OKAY = "b00".U
+    val RESERVED = "b01".U
+    val SLVERR = "b10".U
+    val DECODEERROR = "b11".U
 }
 class avalon_mm_bfm_master_main[T <: MultiIOModule](dut :T, val avm_if: avalon_mm_bfm_master_if) extends PeekPokeTester(dut) {
     // Connect DTU with Avalon Bus Function Model
     //val avm_if = new avalon_mm_bfm_master_if
-    // DUT SIGNALS
-    val addr      = dut.avm_address
-    val byteenable = dut.avm_byteenable
-    val debugaccess = dut.avm_debugaccess
-    val read = dut.avm_read
-    val write = dut.avm_write
-    val writedata = dut.avm_writedata
 
-    val readdata = dut.avm_readata
-    val response = dut.avm_response
-    val waitrequest = dut.avm_waitrequest
+    val addr      = avm_if.avm_address
+    val byteenable = avm_if.avm_byteenable
+    //val debugaccess = avm_if.avm_debugaccess
+    val read = avm_if.avm_read
+    val write = avm_if.avm_write
+    val writedata = avm_if.avm_writedata
+
+    val readdata = avm_if.avm_readdata
+    val response = avm_if.avm_response
+    val waitrequest = avm_if.avm_waitrequest
     val burst_cnt = 0.U
 
+    // DUT SIGNALS
     // Initialization
     // BFM -> DUT signals
     poke(addr,0x0000)
-    peek()
-    // DUT -> BFM signals
 
+    // DUT -> BFM signals
+    def avalon_mm_if_signal_init(addr_w: Int, data_w: Int, burst_w: Int, lock_value: Boolean) : avalon_mm_bfm_master_if = {
+        val result : avalon_mm_bfm_master_if = new avalon_mm_bfm_master_if(addr_w, data_w, burst_w)
+        //BFM TO DUT (Output)
+        result.avm_address      := 0.U(addr_w.W)
+        result.avm_byteenable   := false.B
+        result.avm_read         := false.B
+        result.avm_write        := false.B
+        result.avm_writedata    := 0.U(data_w.W)
+        result.avm_lock         := false.B
+        //DUT to BFM (Input)
+        result.avm_readdata := 0.U(data_w.W)
+        result.avm_response := 0.U(2.W)
+        result.avm_waitrequest := false.B
+        result.avm_readdatavalid := false.B
+
+        result // Return
+    }
+
+    def avalon_mm_if_response_status(response: Int): UInt = {
+        object status extends avm_mm_response_status
+        response match{
+            case 0 => status.OKAY
+            case 1 => status.SLVERR
+            case 2 => status.RESERVED
+            case 3 => status.DECODEERROR
+        }
+    }
     def avalon_mm_wr(waitrequest: Boolean, t: Long) : Unit = {
         val write = peek(avm_if.avm_write) > 0
         if(!waitrequest && write) {
